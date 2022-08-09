38aa3447d1c654c2e7fc58e4587baca11d7ce88f
natesturcke <natesturcke@Nates-Maccy-Mac-2.local>
2022-08-09 08:04:39 +0800

    2022-08-09-1

--- imports/ui/pages/styles/styles.js
+++ imports/ui/pages/styles/styles.js
@@ -2,19 +2,31 @@ import './styles.html';
 import './styles.less';
 
 
+
+
 Template.styles.onRendered(function() {
 
   $(document).ready(function() {
+
+    $('.select2-dropdown').select2({width: '100%' }).on('select2:open', function(e){
+      $('.select2-search__field').attr('placeholder', 'Search...');
+    });
+
+    $('.select2-tags').select2({
+      width: '100%',
+      tags: true
+    });
+
+
+
   })
 });
 
 Template.styles.onCreated(function() {
 
-
 });
 
 Template.styles.helpers({
-
 
 });
 
